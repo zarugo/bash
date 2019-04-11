d9c3bf4a5f2e6b7966e17b1949e4901e229611dd
U-FAACSPA\luigi.zarlenga <luigi.zarlenga@zarlenga-luigi.faacspa.local>
2019-04-11 12:14:31 +0200

    First release

--- beta_udateJPS.sh
+++ beta_udateJPS.sh
@@ -1,9 +1,78 @@
 #!/bin/bash
 WORKDIR=/home/root
 TYPE=$(ps | grep [J]PSApplication | awk '{print $6}')
+ADDJS=/home/root/JPSApps/JPSApplication/Resources/AdditionalData.json
 
-#if [ $(pwd) != $WORKDIR ]
-	# then 
-	# cd $WORKDIR
-# fi
-echo $TYPE
+#double check we are in the correct directory
+if [ $(pwd) != $WORKDIR ]
+	then 
+	cd $WORKDIR
+fi
+#who am I?
+case $TYPE in 
+	AppAps)
+	TOKEN=/home/root/JPSApps/JPSApplication/Resources/ApsApp/AppDB.fdb
+	CASH=/home/root/JPSApps/JPSApplication/Resources/Cash/cashDB.fdb
+	JS=/home/root/JPSApps/JPSApplication/Resources/www/webcfgtool/apsapp/ApsApp/ConfigData.json
+	#TARIFF=/mnt/sd/AslApp/TarDB.xml
+	;;
+	AppLx)
+	TOKEN=/home/root/JPSApps/JPSApplication/Resources/AplApp/AppDB.fdb
+	JS=/home/root/JPSApps/JPSApplication/Resources/www/webcfgtool/aplapp/AplApp/ConfigData.json
+	;;
+	AppLe)
+	TOKEN=/home/root/JPSApps/JPSApplication/Resources/LeApp/AppDB.fdb
+	JS=/home/root/JPSApps/JPSApplication/Resources/www/webcfgtool/leapp/LeApp/ConfigData.json
+	;;
+	
+esac
+
+#kill everything
+pgrep -f AppRun.sh | xargs kill -9 && pgrep JPSApplication | xargs kill -9
+
+#remove old backups
+ls | grep -e [JPSApps]_ | xargs rm -fr
+
+#backup the JBL Token and Cash
+cp $TOKEN .
+if [ $TYPE = AppAps ]
+	then 
+	cp $CASH .
+fi
+#backup json files
+cp $ADDJS .
+cp $JS .
+
+#backup
+mv ./JPSApps ./JPSApps_old
+
+#untar the new package
+tar -xf JPSApps.tar.gz
+chmod +x -R JPSApps
+
+#restore 
+mv ./AppDB.fdb $TOKEN
+if [ -f ./cashDB.fdb ]
+	then 
+	mv ./cashDB.fdb $CASH
+fi
+mv ./AdditionalData.json $ADDJS
+#############################
+#merge JSON TBD
+##############################
+##############################
+mv ./ConfigData.json $JS
+
+#sync ???
+sync
+
+reboot
+
+
+
+
+	
+	
+	
+	
+
