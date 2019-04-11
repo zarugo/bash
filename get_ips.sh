3511bd3bae0eb8651eee9bb159817e8aa1f0fd34
U-FAACSPA\luigi.zarlenga <luigi.zarlenga@zarlenga-luigi.faacspa.local>
2019-04-11 09:49:22 +0200

    First commit

--- get_ips.sh
+++ get_ips.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #commit test :)
+set -x
 
 IPS=$(arp.exe -a |grep 9c-53 |awk '{print $1}')
 TYPE=$(ssh -o "StrictHostKeyChecking no" root@${IPS} "ps |grep "[J]PSApplication" |awk '{print \$6}'")
