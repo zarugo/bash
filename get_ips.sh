57497141bfc6faa05fe14282b1a7963156342aa1
zar <zar@zar>
2019-04-11 09:29:20 +0200

    Rename script

---
+++ get_ips.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#commit test :)
+
+IPS=$(arp.exe -a |grep 9c-53 |awk '{print $1}')
+TYPE=$(ssh -o "StrictHostKeyChecking no" root@${IPS} "ps |grep "[J]PSApplication" |awk '{print \$6}'")
+for i in $IPS
+	do 
+		echo "${IPS}_${TYPE}"
+	done
