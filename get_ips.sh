8659eafbde7f4cf2e8cc2d5e6e131cf6da2176eb
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2019-05-28 14:16:47 +0200

    x

--- get_ips.sh
+++ get_ips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #commit test  2 ;)
-set -x
+#set -x
 
 IPS=$(arp.exe -a |grep 9c-53 |awk '{print $1}')
 TYPE=$(ssh -o "StrictHostKeyChecking no" root@${IPS} "ps |grep "[J]PSApplication" |awk '{print \$6}'")
