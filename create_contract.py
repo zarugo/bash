f7bc3b8c99821285ea150b5ee6b04dcf65e3f772
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2019-12-03 14:02:15 +0100

    added a check on response code from JMS so we now that we have errors on standard output

--- create_contract.py
+++ create_contract.py
@@ -3,14 +3,9 @@
 # cardNumber,mediaType,start_validity,end_validity,enabled,accessPolicyId,customerFirstName,CustomerLastName,displayCardNumber
 # 123456,proximity,1559347200000,2556057600000,TRUE,9,Bob,Marley,
 
-
-
-
-
-
 import os, sys, csv, json, requests, datetime
 
-
+#get the data we need
 now = datetime.datetime.now()
 jms = input("Type the ip of JMS:  ")
 username = input("Type the username of the third party:  ")
@@ -22,7 +17,7 @@ while True:
     else:
         break
 
-# Do the login and get the token every time
+# Do the login and store the token every time
 
 auth_url = "http://" + jms + ":8080/janus-integration/api/ext/login"
 log_headers = { "Content-Type": "application/json" , "Accept": "application/json"}
@@ -47,7 +42,7 @@ with open(input_file) as csvfile:
         csv_lines = {}
         nested = ["start_validity" , "end_validity" , "enabled"]
         csv_lines = {k: v for k, v in line.items() if k not in nested}
-        csv_lines["cardParameters"] = [{'name': k, 'value': v} for k, v in line.items() if k in nested]
+        csv_lines["cardParameters"] = [{'type': k, 'value': v} for k, v in line.items() if k in nested]
         create_url = "http://" + jms + ":8080/janus-integration/api/ext/card/create"
         headers = { "Content-Type": "application/json" , "Accept": "application/json", "Janus-TP-Authorization": token }
         data = (json.dumps(csv_lines, sort_keys=False, indent=4, separators=(",",": "), ensure_ascii=False))
@@ -56,8 +51,12 @@ with open(input_file) as csvfile:
             r = requests.post(create_url, headers=headers, data=data, timeout=10.0)
             with open("create_contract_results.log", "a") as log:
                 log.write( now.strftime("%Y-%m-%d %H:%M:%S") + "  Contract inserted: " + r.text + ' ' + "Response Code from JMS was: " + str(r.status_code) + "\n" )
+                if r.status_code != 200:
+                    print("An error occurred creating the contract for " + csv_lines["customerFirstName"] + ' ' + csv_lines["customerLastName"] + ", please check the log file")
+                else:
+                    print("Created contract for " + csv_lines["customerFirstName"] + csv_lines["customerLastName"])
         except Exception as e:
-            print("Soething went wrong, the error is " + str(e))
+            print("Something went wrong, the error is " + str(e))
 
 
 print("The upload is done, please check on JMS that the customers are there. The log file is upload_results.log.")
