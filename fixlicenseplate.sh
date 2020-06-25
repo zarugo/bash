975a906a0235a25f2f17a612733f6be1d9fe2df9
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2020-06-25 13:28:38 +0200

    first upload

---
+++ fixlicenseplate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PGPASSWORD=jbl
+export DB_NAME=jbl
+
+while true; do
+	psql -U jbl -c "update lpr_camera_recognitions set original_licence_plate = null where original_licence_plate = '-1' and corrected_licence_plate is null"
+	sleep 20
+done
+	
