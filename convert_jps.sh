07abeb233a735cf18c852ce5033840dbefd65bdf
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2020-07-14 10:06:58 +0200

    upload

---
+++ convert_jps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+[[ $# -ne 1 ]] && echo -e "\n  Please use \"old\" or \"new\" as an argument" || :
+
+if [[ $1 == "new" ]]; then
+  for i in 170 180 190 200 210; do
+    ssh root@172.29.0.$i 'rm JPSApps; ln -s JPSApps_1.10.4 JPSApps; reboot'
+  done
+  exit 0
+elif [[ $1 == "old" ]]; then
+  for i in 170 180 190 200 210; do
+    ssh root@172.29.0.$i 'rm JPSApps; ln -s JPSApps_1.8.5 JPSApps; reboot'
+  done
+  exit 0
+fi
