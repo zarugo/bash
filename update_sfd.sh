688cbc8445e0c3cf66f0458692426d953c7c9b4e
Luigi Zarlenga <luigi.zarlenga@faacspa.local>
2020-10-23 17:03:40 +0200

    prepare it for production

--- update_sfd.sh
+++ update_sfd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#set -x
-
+set -x
+shopt -s expand_aliases
 ###################################################################
 #Script Name	: update_sfd.sh
 #Description	: Automatic update tool to update screens from devices
@@ -9,20 +9,25 @@
 ###################################################################
 
 username='root'
-password=$2
 ip=$1
-chmod +x ./sshpass
+password=$2
+
+#prepare the tools
+
+chmod +x /home/root/JPSApps/libs/sshpass
+alias sshpass='/home/root/JPSApps/libs/sshpass'
+mv /home/root/JPSApps/JPSApplication/Resources/xegu/ /mnt/sd/ 2>/dev/null
+
 #prevent known_hosts issues: if the argument is an IP, delete only the public key of that IP, so we do not affect other ssh sessions
 sed -i -E "s/$1.+//" ~/.ssh/known_hosts
 
 
-
 #Get the display size
 function get_type () {
-  type=$(./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'fbset | grep "mode " | awk "{print \$2}" | tr -d "\"" | cut -d "-" -f1')
+  type=$(sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'fbset | grep "mode " | awk "{print \$2}" | tr -d "\"" | cut -d "-" -f1')
   #check if we have the xegu_ul binary
   if [[ $type == '800x480' ]]; then
-    ul=$(./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'ls /home/root/xegu_ul 2>/dev/null')
+    ul=$(sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'ls /home/root/xegu_ul 2>/dev/null')
   fi
 }
 
@@ -46,7 +51,7 @@ EOF
 }
 
 function update_binary () {
-  ./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'find /home/root/ -regex ".*xegu.*old" | xargs rm'
+  sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'find /home/root/ -regex ".*xegu.*old" | xargs rm'
   #TBD check if we have space - for some reason, the space is very little but we are able to upload the files anyway
   #occupied=$(sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'df -h | grep ubi0 | awk "{print \$5}" | cut -d "%" -f1')
   # if [[ $occupied -ge 88 ]]; then
@@ -58,16 +63,16 @@ function update_binary () {
   ####################################################################################
   #if we have the xegu_ul binary, we store it as xegu.old and push the xegu binary
  if [[ $ul ]]; then
-    ./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'mv /home/root/xegu_ul /home/root/xegu'
+    sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'mv /home/root/xegu_ul /home/root/xegu'
     xegu_script
-    ./sshpass -p $password scp -o "StrictHostKeyChecking no" xegu.sh $username@$ip:/home/root/xegu.sh
-    ./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'chmod +x /home/root/xegu.sh'
+    sshpass -p "$password" scp -o "StrictHostKeyChecking no" xegu.sh "$username"@"$ip":/home/root/xegu.sh
+    sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'chmod +x /home/root/xegu.sh'
     rm -f xegu.sh
   fi
-  ./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'mv /home/root/xegu /home/root/xegu.old'
-  ./sshpass -p $password scp -o "StrictHostKeyChecking no" $binary $username@$ip:/home/root/xegu
-  ./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'chmod +x xegu'
-  ./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'reboot'
+  sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'mv /home/root/xegu /home/root/xegu.old'
+  sshpass -p "$password" scp -o "StrictHostKeyChecking no" "$binary" "$username"@"$ip":/home/root/xegu
+  sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'chmod +x xegu'
+  sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'reboot'
 
 }
 
@@ -82,13 +87,13 @@ case $type in
   xegulang="/home/root/JPSApps/JPSApplication/Resources/xegulang.xml"
   ;;
   1366x768 )
-  binary="/mnt/sd/xegu/big/xegu"
-  xegulang="/home/root/JPSApps/JPSApplication/Resources/xegulang.xml"
-  ;;
+  ;&
+  1368x768 )
+  ;&
   1280x800 )
-  binary="/mnt/sd/xegu/big/xegu"
-  xegulang="/home/root/JPSApps/JPSApplication/Resources/xegulang.xml"
-  ;;
+  ;&
+  1376x768 )
+  ;&
   1920x1080 )
   binary="/mnt/sd/xegu/big/xegu"
   xegulang="/home/root/JPSApps/JPSApplication/Resources/xegulang.xml"
@@ -96,15 +101,31 @@ case $type in
 
 esac
 
-#check if we need the update the language, do the update only if needed
-currentlang=$(./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'md5sum /home/root/.local/share/xegu/xegulang.xml 2>/dev/null | cut -d " " -f1')
+#use hash to check if we need the update the language, do the update only if needed
+currentlang=$(sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username@$ip" 'md5sum /home/root/.local/share/xegu/xegulang.xml 2>/dev/null | cut -d " " -f1')
 newlang=$(md5sum $xegulang | cut -d " " -f1)
-[[ $currentlang == $newlang ]] && langupdate='' || langupdate=yes
-[[ $langupdate ]] && ./sshpass -p $password scp -o "StrictHostKeyChecking no" $xegulang $username@$ip:/home/root/.local/share/xegu/xegulang.xml || :
+if [[ $currentlang == "$newlang" ]]; then
+  langupdate=''
+else
+  langupdate=yes
+fi
+if [[ $langupdate ]]; then
+  sshpass -p "$password" scp -o "StrictHostKeyChecking no" "$xegulang" "$username"@"$ip":/home/root/.local/share/xegu/xegulang.xml
+else
+  :
+fi
 
 
-#check if we need the update the binary, do the update only if needed
-currentbin=$(./sshpass -p $password ssh -o "StrictHostKeyChecking no" $username@$ip 'md5sum xegu 2>/dev/null | cut -d " " -f1')
-newbin=$(md5sum $binary | cut -d " " -f1)
-[[ $currentbin == $newbin ]] && binupdate='' || binupdate=yes
-[[ $binupdate ]] && update_binary  || :
+#use hash to check if we need the update the binary, do the update only if needed
+currentbin=$(sshpass -p "$password" ssh -o "StrictHostKeyChecking no" "$username"@"$ip" 'md5sum xegu 2>/dev/null | cut -d " " -f1')
+newbin=$(md5sum "$binary" | cut -d " " -f1)
+if [[ $currentbin == "$newbin" ]]; then
+  binupdate=''
+else
+  binupdate=yes
+fi
+if [[ $binupdate ]]; then
+  update_binary
+else
+  :
+fi
